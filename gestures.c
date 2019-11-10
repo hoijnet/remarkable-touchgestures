051182a2bf89bddacd36b6abc680b74e36ea5c6f
Philippe Höij <philippe@hoij.net>
2019-11-10 23:22:02 +0100

    Changed the swipe min distance

--- gestures.c
+++ gestures.c
@@ -87,12 +87,12 @@ void recognize_gestures(struct TouchEvent *f) {
                         //swipe 
                         if (abs(dx) > abs(dy)) {
                             //horizontal
-                            if (dx < 0 && dx < -300) {
+                            if (dx < 0 && dx < -500) {
                                 printf("swipe left\n");
 								gesture.type = SwipeLeft; 
 								interpret_gesture(&gesture);
                             }
-                            else if (dx > 0 && dx > 300) {
+                            else if (dx > 0 && dx > 500) {
                                 printf("swipe right\n");
                                 gesture.type = SwipeRight; 
                                 interpret_gesture(&gesture);
